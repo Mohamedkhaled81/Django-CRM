3fc119512716a23d10f20c8ea2cb960130f063ba
Mohamed Zakaria <medozakaria014@gmail.com>
2023-05-16 16:15:21 +0200

    "Finish work with Leads operations and templates"

--- CRM/lead/views.py
+++ CRM/lead/views.py
@@ -13,7 +13,7 @@ def create_lead(request):
             obj = form.save(commit=False)
             obj.created_by = request.user
             obj.save() 
-            messages.add_message(request, messages.SUCCESS, "The Lead has Succefuly Created")
+            messages.add_message(request, messages.SUCCESS, "The Lead was created")
             return redirect('list_leads')
         
     else:    
@@ -45,14 +45,16 @@ def mod_lead(request,id):
         mod_form = CreateLead(request.POST,instance=lead)
         if mod_form.is_valid():
             mod_form.save()
-            return redirect('dashboard')
+            messages.add_message(request, messages.SUCCESS, "The Lead was modified")
+            return redirect('detail_lead',id = id)
 
     mod_form = CreateLead(instance=lead)    
-    return HttpResponse("tmm")
+    return render(request,'lead/modify_lead.html',{'form':mod_form})
     
 
 @login_required
 def delete_lead(request,id):
     lead = get_object_or_404(Leads,created_by = request.user,id = id)
     lead.delete()
-    return redirect('dashboard')
+    messages.add_message(request, messages.SUCCESS, "The Lead was deleted")
+    return redirect('list_leads')
