530852498abc837245e8dd9cc13d67ac52dfd4f1
Mohamed Zakaria <medozakaria014@gmail.com>
2023-05-16 05:37:51 +0200

    "Create views for listing and updating and deleting and modifying for the lead"

--- CRM/lead/urls.py
+++ CRM/lead/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.list_leads, name='list_leads'),
     path('create/', views.create_lead, name='create_lead'),
-
+    path('<int:id>/', views.detail_lead, name='detail_lead'),
+    path('<int:id>/modify', views.mod_lead, name='mod_lead'),
+    path('<int:id>/delete', views.delete_lead, name='delete_lead'),
 ]
--- CRM/lead/views.py
+++ CRM/lead/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponse
 from .forms import CreateLead
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Leads
 # Create your views here.
 
 @login_required
@@ -11,7 +13,46 @@ def create_lead(request):
             obj = form.save(commit=False)
             obj.created_by = request.user
             obj.save() 
-            return redirect('dashboard')
+            messages.add_message(request, messages.SUCCESS, "The Lead has Succefuly Created")
+            return redirect('list_leads')
+        
     else:    
         form = CreateLead()
     return render(request,'lead/create_lead.html',{'form':form})    
+
+
+
+@login_required
+def list_leads(request):
+    leads = Leads.objects.filter(created_by = request.user)
+    if leads:
+        return render(request,'lead/list_leads.html',{'leads':leads})
+    return render(request,'lead/list_leads.html')
+
+
+@login_required
+def detail_lead(request,id):
+    lead = get_object_or_404(Leads,created_by = request.user,id = id)
+    return render(request,'lead/detail_lead.html',{'lead':lead})    
+
+
+
+
+@login_required
+def mod_lead(request,id):
+    lead = get_object_or_404(Leads,created_by = request.user,id = id)
+    if request.method == 'POST':
+        mod_form = CreateLead(request.POST,instance=lead)
+        if mod_form.is_valid():
+            mod_form.save()
+            return redirect('dashboard')
+
+    mod_form = CreateLead(instance=lead)    
+    return HttpResponse("tmm")
+    
+
+@login_required
+def delete_lead(request,id):
+    lead = get_object_or_404(Leads,created_by = request.user,id = id)
+    lead.delete()
+    return redirect('dashboard')
